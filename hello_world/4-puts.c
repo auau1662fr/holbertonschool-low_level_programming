b7439011312cd8ff743b88d0bda2aa9f8aeb96ea
auau1662fr <auric.chen.2005@gmail.com>
2025-02-17 14:55:56 +0100

    holbertonschool-low_level_programming

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ *main -Entry point
+
+ * Description:prints "programming is like building a multilingual puzzle"
+ *               using the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    puts("La programmation, c'est comme construire un puzzle multilingue");
-    return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
