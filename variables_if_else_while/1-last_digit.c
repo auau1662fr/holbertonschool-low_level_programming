c1faf581f308efe07acd0b77e2bfbb9c05a4a8be
auau1662fr <au ric.chen.2005@gmail.com>
2025-02-18 13:52:48 +0100

    variables_if_else_while

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,25 +12,31 @@
  */
 int main(void)
 {
-int n;
-int dernier_chiffre;
-srand(time(0));  // Initialise le générateur de nombres aléatoires
-n = rand() - RAND_MAX / 2;  // Génère un nombre aléatoire
-dernier_chiffre = n % 10;  // Obtenez le dernier chiffre du nombre
-// Affiche la première partie de la sortie
-printf("Dernier chiffre de %d est %d", n, dernier_chiffre);
-// Vérifie la valeur du dernier chiffre et affiche le message correspondant
-if (dernier_chiffre > 5)
-{
-printf(" et est supérieur à 5\n");
+    int n;
+    int dernier_chiffre;
+
+    srand(time(0)); /* Initialise le générateur de nombres aléatoires */
+    n = rand() - RAND_MAX / 2; /* Génère un nombre aléatoire */
+
+    dernier_chiffre = n % 10; /* Obtenez le dernier chiffre du nombre */
+
+    /* Affiche la première partie de la sortie */
+    printf("Dernier chiffre de %d est %d", n, dernier_chiffre);
+
+    /* Vérifie la valeur du dernier chiffre et affiche le message correspondant */
+    if (dernier_chiffre > 5)
+    {
+        printf(" et est supérieur à 5\n");
+    }
+    else if (dernier_chiffre == 0)
+    {
+        printf(" et est 0\n");
+    }
+    else
+    {
+        printf(" et est inférieur à 6 et différent de 0\n");
+    }
+
+    return 0;
 }
-else if (dernier_chiffre == 0)
-{
-printf(" et est 0\n");
-}
-else
-{
-printf(" et est inférieur à 6 et différent de 0\n");
-}
-return 0;
-}
+
