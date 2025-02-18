b15f99ca516acc34b377df5e91a162e2e7854c0f
auau1662fr <au ric.chen.2005@gmail.com>
2025-02-18 10:03:21 +0100

    variables_if_else_while

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,5 @@
-julien@ubuntu:~/$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-positive_or_negative.c -o 0-positive_or_negative
+julien@ubuntu:~/$ gcc -Wall -pedantic -Werror -Wextra -
+std=gnu89 0-positive_or_negative.c -o 0-positive_or_negative
 julien@ubuntu:~/$ ./0-positive_or_negative 
 -520693284 is negative
 julien@ubuntu:~/$ ./0-positive_or_negative 
@@ -15,4 +16,4 @@ julien@ubuntu:~/$ ./0-positive_or_negative
 -48147767 is negative
 julien@ubuntu:~/$ ./0-positive_or_negative 
 0 is zero
-julien@ubuntu:~/$
+julien@ubuntu:~/$ 
