cec23b27691734b9e4ac7477c29b26b4d55e8861
auau1662fr <au ric.chen.2005@gmail.com>
2025-02-19 12:47:12 +0100

    Task 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,23 +10,19 @@
 
 int main(void)
 {
-	int num1, num2;
+	int num;
 
-	for (num1 = 0; num1 < 9; num1++)
-	{	
-	for (num2 = num1 + 1; num2 < 10; num2++)
+	for (num = 0; num < 10; num++)
 	{
-	putchar(num1 + '0');
-	putchar(num2 + '0');
-	if (!(num1 == 8 && num2 == 9))
+	putchar(num + '0');
+	if (num != 9)
 	{
 	putchar(',');
 	putchar(' ');
-	}
-	}
+        }
 	}
 	putchar('\n');
 
-	return 0;
+	return (0);
 }
 
