e3219c8ba6d45d6897f44458fe294c5c6215d8df
auau1662fr <au ric.chen.2005@gmail.com>
2025-02-19 11:49:07 +0100

    Task 8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -3,7 +3,7 @@
 /**
 * main - Entry point of the program
 *
-* Description: prints all the numbers of base 16 in lowercase, 
+* Description: prints all the numbers of base 16 in lowercase
 *
 * Return: Always 0 (Success)
 */
