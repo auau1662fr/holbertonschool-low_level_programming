453fb23ec5abce9f13de34c796de301f803896b0
auau1662fr <au ric.chen.2005@gmail.com>
2025-02-19 11:46:20 +0100

    Task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of the program
+*
+* Description: prints all the numbers of base 16 in lowercase, 
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 16; num++)
+	{
+	if (num < 10)
+	putchar(num + '0');
+	else
+	putchar(num - 10 + 'a');
+	}
+	putchar('\n');
+
+	return (0);
+}
+
