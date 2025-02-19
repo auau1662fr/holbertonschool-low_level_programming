da897dc79a4496d7990848f941dda6430dd95b34
auau1662fr <au ric.chen.2005@gmail.com>
2025-02-19 15:59:52 +0100

    Task 4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -6,16 +6,19 @@
 * Description: Print all the letters except q and e
 *
 * Return: Always 0 (Success)
-*/+
+*/
+
 int main(void)
 {
+
 	int i = 97;
 
-	do {
+	while (i <= 122)
+	{
 	if (i != 101 && i != 113)
-	putchar(i);
+		putchar(i);
 	i++;
-	} while (i <= 122);
+	}
 
 	putchar('\n');
 	return (0);
