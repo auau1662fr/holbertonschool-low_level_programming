552de06aac270980369edff740ae9be8d9d2a094
auau1662fr <au ric.chen.2005@gmail.com>
2025-02-19 09:09:42 +0100

    variables_if_else_while

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Print all the letters except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		char c;
+		for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+	{
+		putchar(c);
+	}
+	}
+		putchar('\n');
+		return (0);
+}
+
