8ad0db05ef85638ab50371284ef17a0996c4a575
auau1662fr <au ric.chen.2005@gmail.com>
2025-02-21 14:38:10 +0100

    Update - Function to print alhabet in lowercase

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ *
+ * print_alphabet -  function that prints the alphabet
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,10 +1,14 @@
 #include <unistd.h>
 
 /**
- * _putchar - Écrit un caractère sur la sortie standard
- * @c: Le caractère à afficher
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-void _putchar(char c)
+
+int _putchar(char c)
 {
-    write(1, &c, 1);  // Écrit le caractère sur la sortie standard
+	return (write(1, &c, 1));
 }
