5dace110b6a7871c8a26bc526fc29783f87b6503
auau1662fr <au ric.chen.2005@gmail.com>
2025-02-21 11:18:47 +0100

    functions_nested_loops

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -8,26 +8,20 @@
 * Return: Always 0 (Success)
 */
 
-void _putchar(char c)
-
-{
-    write(1, &c, 1);
-}
-
 void print_alphabet(void)
+int main(void)
+
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)  
 	{
 	_putchar(letter);
 	}
+
 	_putchar('\n');
 }
-
-int main(void)
 {
 	print_alphabet();
 	return (0);
 }
-
