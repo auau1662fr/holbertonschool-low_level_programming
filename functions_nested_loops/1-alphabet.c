f9c92f597f9f5c2f46216cfef5d56e6e0e826448
auau1662fr <au ric.chen.2005@gmail.com>
2025-02-21 13:28:02 +0100

    functions_nested_loops

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -9,17 +9,15 @@
 */
 
 void _putchar(char c)
-
 {
 	write(1, &c, 1);
 }
 
 void print_alphabet(void)
-
 {
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
-	_putchar(alphabet[0]);
-	for (int i = 1; alphabet[i] != '\0'; i++) {
+
+	for (int i = 0; alphabet[i] != '\0'; i++) {
 	_putchar(alphabet[i]);
 	}
 }
