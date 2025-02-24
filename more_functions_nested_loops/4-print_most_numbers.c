9edb09f95e30162660f8dead8f34422762c8a1a2
auau1662fr <au ric.chen.2005@gmail.com>
2025-02-24 14:43:21 +0100

    more_functions_nested_loops 4.0

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,17 +2,22 @@
 
 /**
  * print_most_numbers - Prints numbers from 0 to 9, except 2 and 4.
+ * This function prints each digit from '0' to '9', except
+ * for '2' and '4'.It uses a loop to check each digit and
+ * skips '2' and '4'.After printing, it prints a newline
+ * to format the output.
  * Return: void (nothing).
  */
 void print_most_numbers(void)
 {
 	char c;
 
-	for (c = '0'; c <= '9'; c++)  
+	for (c = '0'; c <= '9'; c++)
 	{
-	if (!(c == '2' || c == '4'))  // Skip '2' and '4'
-	_putchar(c);  // Print the current character
+	/* Skip '2' and '4' */
+	if (!(c == '2' || c == '4'))
+	_putchar(c);  /* Print the current character */
 	}
-	_putchar('\n');  // Print a newline after the numbers
+	_putchar('\n');  /* Print a newline after the numbers */
 }
 
