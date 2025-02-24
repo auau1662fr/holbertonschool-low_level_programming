601aa5fc7f27cc72bfa73f431476d3073d635863
auau1662fr <au ric.chen.2005@gmail.com>
2025-02-24 11:07:23 +0100

    more_functions_nested_loops 0.0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,9 @@
+#include <main.h>
+
+int _isupper(int c) {
+    if (c >= 'A' && c <= 'Z') {
+        return 1;  // Le caractère est en majuscule
+    }
+    return 0;  // Le caractère n'est pas en majuscule
+}
+
