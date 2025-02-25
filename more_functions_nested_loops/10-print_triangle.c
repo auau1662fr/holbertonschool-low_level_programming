6d06039540e25804025bbc43589821cfce311f2b
auau1662fr <au ric.chen.2005@gmail.com>
2025-02-25 14:30:29 +0100

    more_functions_nested_loops10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -6,23 +6,23 @@
  */
 void print_triangle(int size)
 {
-    int i, j, k;
+	int i, j, k;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+	if (size <= 0)
+	{
+	_putchar('\n');
+	return;
+	}
 
-    for (i = 1; i <= size; i++)
-    {
-        for (j = 1; j <= size - i; j++)
-            _putchar(' ');
-        
-        for (k = 1; k <= i; k++)
-            _putchar('#');
+	for (i = 1; i <= size; i++)
+	{
+	for (j = 1; j <= size - i; j++)
+	_putchar(' ');
 
-        _putchar('\n');
-    }
+	for (k = 1; k <= i; k++)
+	_putchar('#');
+
+	_putchar('\n');
+	}
 }
 
