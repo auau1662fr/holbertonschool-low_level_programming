cf911d0cc8bb4b5f37f81a5f52cf3ccc2e9fcee3
auau1662fr <au ric.chen.2005@gmail.com>
2025-02-25 12:32:00 +0100

    more_functions_nested_loops10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -6,26 +6,26 @@
  */
 void print_triangle(int size)
 {
-    int c, i, j;
+	int c, i, j;
 
-    if (size <= 0) {
-        _putchar('\n');
-        return;
-    }
+	if (size <= 0) {
+	_putchar('\n');
+	return;
+	}
 
-    for (c = 0; c < size; c++) {
-        // Print spaces
-        for (i = 0; i < size - c - 1; i++) {
-            _putchar(' ');
-        }
+	for (c = 0; c < size; c++) {
+	// Print spaces
+	for (i = 0; i < size - c - 1; i++) {
+	_putchar(' ');
+	}
 
-        // Print hashes
-        for (j = 0; j < c + 1; j++) {
-            _putchar('#');
-        }
+	// Print hashes
+	for (j = 0; j < c + 1; j++) {
+	_putchar('#');
+	}
 
-        // Print new line after each row
-        _putchar('\n');
-    }
+	// Print new line after each row
+	_putchar('\n');
+	}
 }
 
