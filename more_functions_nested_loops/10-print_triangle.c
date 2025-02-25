1af5fa8e3903fd5f5876fbd13f3187e68b880b57
auau1662fr <au ric.chen.2005@gmail.com>
2025-02-25 12:28:53 +0100

    more_functions_nested_loops10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle aligned right, using '#'
+ * @size: Size of triangle
+ */
+void print_triangle(int size)
+{
+    int c, i, j;
+
+    if (size <= 0) {
+        _putchar('\n');
+        return;
+    }
+
+    for (c = 0; c < size; c++) {
+        // Print spaces
+        for (i = 0; i < size - c - 1; i++) {
+            _putchar(' ');
+        }
+
+        // Print hashes
+        for (j = 0; j < c + 1; j++) {
+            _putchar('#');
+        }
+
+        // Print new line after each row
+        _putchar('\n');
+    }
+}
+
