2950c82fd53b193792303302a86b445459d2fb5e
auau1662fr <au ric.chen.2005@gmail.com>
2025-02-26 11:47:45 +0100

    pointers_arrays_strings1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,20 +1,17 @@
-#include <main.h>
+#include "main.h"
 
+/**
+ * swap_int - Function to execute in the program
+ * @a: first integrer
+ * @b: second integrer
+ *
+ * Description: Function to swap the value of two integrer
+ */
 void swap_int(int *a, int *b)
 {
-	int temp = *a;  /* Stocke la valeur de *a dans temp */
-	*a = *b;  /* Assigne la valeur de *b à *a */
-	*b = temp;  /* Assigne la valeur de temp (ancien *a) à *b */
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
 }
-
-int main(void)
-{
-	int x = 5, y = 10;
-
-	printf("Before swap: x = %d, y = %d\n", x, y);
-	swap_int(&x, &y);  /* Passe les adresses de x et y */
-	printf("After swap: x = %d, y = %d\n", x, y);
-
-	return (0);
-}
-
