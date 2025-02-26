88c14e7791f83a01a8e6ed4d656cae64abc52363
auau1662fr <au ric.chen.2005@gmail.com>
2025-02-26 11:35:25 +0100

    pointers_arrays_strings1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,16 +1,18 @@
-#include "main.h"
+#include <main.h>
 
-void swap_int(int *a, int *b) {
-    int temp = *a;  // Stocke la valeur de *a dans temp
-    *a = *b;        // Assigne la valeur de *b à *a
-    *b = temp;      // Assigne la valeur de temp (ancien *a) à *b
+void swap_int(int *a, int *b)
+{
+	int temp = *a;  /* Stocke la valeur de *a dans temp */
+	*a = *b;  /* Assigne la valeur de *b à *a */
+	*b = temp;  /* Assigne la valeur de temp (ancien *a) à *b */
 }
 
-int main() {
+int main(void)
+{
 	int x = 5, y = 10;
 
 	printf("Before swap: x = %d, y = %d\n", x, y);
-	swap_int(&x, &y);  // Passe les adresses de x et y
+	swap_int(&x, &y);  /* Passe les adresses de x et y */
 	printf("After swap: x = %d, y = %d\n", x, y);
 
 	return (0);
