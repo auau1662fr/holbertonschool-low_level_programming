69ca7033e6f728d185692a0522ed28521ad2711e
auau1662fr <auric.chen.2005@gmail.com>
2025-03-02 17:00:52 +0100

    pointers_arrays_strings

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,21 +8,20 @@
 
 char *leet(char *s)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
-	}
-
-	return (s);
+    int i;
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] == 'a' || s[i] == 'A')
+            s[i] = '4';
+        else if (s[i] == 'e' || s[i] == 'E')
+            s[i] = '3';
+        else if (s[i] == 'o' || s[i] == 'O')
+            s[i] = '0';
+        else if (s[i] == 't' || s[i] == 'T')
+            s[i] = '7';
+        else if (s[i] == 'l' || s[i] == 'L')
+            s[i] = '1';
+    }
+    return s;
 }
+
