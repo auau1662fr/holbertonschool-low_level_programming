1f2e95b1006eb6c77d19316ca20dc905b35c1589
romain344 <romainleoulzec@gmail.com>
2025-11-06 10:03:05 +0100

    test 1-print_rev_recursion.c

--- recursion/main.h
+++ recursion/main.h
@@ -4,4 +4,6 @@
 void _puts_recursion(char *s);
 int _putchar (char c);
 void _print_rev_recursion(char *s);
+void print_diagsums(int *a, int size);
+char *_strstr(char *haystack, char *needle);
 #endif
