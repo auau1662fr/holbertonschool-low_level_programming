fb8106abe630e5c4172320c9870daf294c564b5c
romain344 <romainleoulzec@gmail.com>
2025-11-12 10:08:55 +0100

    modification 0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -22,7 +22,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	arr = malloc(sizeof(char) * (size + 1));
+	arr = malloc(sizeof(char) * size);
 	if (arr == NULL)
 	{
 		return (NULL);
