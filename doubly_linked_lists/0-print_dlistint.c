00f23a62fc633339e857edd410fa9cb9eae573cb
auau1662fr <auric.chen.2005@gmail.com>
2025-12-03 12:13:53 +0100

    tset 0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -9,15 +9,15 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *node = h;
-	size_t cont = 0;
+    const dlistint_t *node = h;
+    size_t cont = 0;
 
-	while (node)
-	{
-		printf("%i\n", node->n);
-		cont++;
-		node = node->next;
-	}
+    while (node)
+    {
+        printf("%i\n", node->n);
+        cont++;
+        node = node->next;
+    }
 
-	return (cont);
+    return (cont);
 }
